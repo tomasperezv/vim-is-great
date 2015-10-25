f3740d647a04c7b5a5b004a919f5e6c0d1e749ab
Tomas Perez <tom@0x101.com>
2015-10-25 14:47:46 +0100

    Update curl-install.sh

--- curl-install.sh
+++ curl-install.sh
@@ -7,7 +7,7 @@
 
 ## Step 1: Clone the repo and setup
 #git clone git@github.com:tomasperezv/vim-is-great.git
-cd $PWD/vim-is-great
+#cd $PWD/vim-is-great
 #sh setup.sh
 
 ## Step 2: Update the .vimrc and .vim files
