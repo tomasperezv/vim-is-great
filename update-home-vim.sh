b97761289fd737af8865f196f8bef838696d66ce
Tomas Perez <tom@0x101.com>
2015-10-25 14:08:07 +0100

    Updating broken script.

--- update-home-vim.sh
+++ update-home-vim.sh
@@ -4,5 +4,5 @@
 # vim-is-great project folder.
 # @see https://github.com/tomasperezv/vim-is-great
 # -----------------------------------------------------------------------------
-ln -s $(./vimrc) $HOME/.vimrc
-ln -s $(./vim) $HOME/vim
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/vim $HOME/vim
