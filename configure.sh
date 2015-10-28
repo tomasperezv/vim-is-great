f76c3d32304f3e1a992c77da3eea45bd2892d3f9
Tomas Perez <tom@0x101.com>
2015-10-28 22:03:59 +0100

    Adding script for keeping track of the dependencies.

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Install the dependencies.
+# @see https://github.com/tomasperezv/vim-is-great
+# -----------------------------------------------------------------------------
+sudo apt-get install -y curl cmake python-dev git silversearcher-ag
