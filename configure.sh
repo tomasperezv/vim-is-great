47e10294625aff99f328b45416501449dd6ce8ad
Tomas Perez <tom@0x101.com>
2015-11-01 19:38:39 +0100

    Adding dependency to the configuration script.

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@
 # Install the dependencies.
 # @see https://github.com/tomasperezv/vim-is-great
 # -----------------------------------------------------------------------------
-sudo apt-get install -y curl cmake python-dev git silversearcher-ag
+sudo apt-get install -y curl cmake python-dev git silversearcher-ag exuberant-ctags
