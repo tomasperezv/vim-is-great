04aa0b55566a762f34a49ad8741286319ecbf3dc
Tomas Perez <tom@0x101.com>
2015-11-03 21:57:20 +0100

    Adding descriptive message to the update script.

--- update.sh
+++ update.sh
@@ -2,12 +2,16 @@
 # -----------------------------------------------------------------------------
 # @see https://github.com/tomasperezv/vim-is-great
 # -----------------------------------------------------------------------------
+folder='./vim/bundle/'
+submodules_count=$(find $folder -maxdepth 1 -not -path $folder -type d | wc -l)
+counter=1
 for dir in ./vim/bundle/*/
 do
-  echo $dir
+  echo '## Updating' $counter 'of' $submodules_count '('$dir')'
   cd $dir
   git branch
   git pull
   git checkout master
   cd -
+  counter=$((counter+1))
 done
